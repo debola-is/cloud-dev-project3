734f52e9c23373b0377c987f58f29a74c211c93b
Debola <adebolasanni079@gmail.com>
2022-09-05 08:33:56 +0100

    minor edits

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a77e5624cd6d04c6a989dcffcdcd440c-76992330.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
